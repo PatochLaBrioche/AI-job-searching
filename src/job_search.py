6957d26e594b5242fd900f024335c12e9ccb7398
TheXdreams <jas.bessier@gmail.com>
2025-01-28 19:41:08 +0100

    stopped at loop checking

--- src/job_search.py
+++ src/job_search.py
@@ -2,8 +2,21 @@ import requests
 import logging
 from src.config import CLIENT_ID, CLIENT_SECRET, SCOPES
 
+# code INSEE de la commune (ici Toulouse)
+commune = 31555
+# distance en km autour de la commune
+distance = 30
+# grand domaine de l'offre d'emploi (ici Informatique / Télécommunication)
+grandDomaine = "M18"
+# motsCles nécessite une string sous la forme 'mot1,mot2,mot3'
+motsCles = "développeur,html,css"
+# Nature contrat
+typeContrat = "CDI"
+# Trie pertinence décroissante, distance croissante, date de création horodatée décroissante, origine de l'offre : sort=0 
+sort = 0
+
 # Fonction pour rechercher des offres d'emploi sur Pôle Emploi
-def search_pole_emploi_jobs(keywords, location, access_token):
+def search_pole_emploi_jobs(keywords, access_token):
     url = "https://api.francetravail.io/partenaire/offresdemploi/v2/offres/search"
     headers = {
         'Accept': 'application/json',
@@ -13,10 +26,12 @@ def search_pole_emploi_jobs(keywords, location, access_token):
     job_offers = []
     for keyword in keywords:
         params = {
-            'q': keyword,
-            'l': location,
-            'distance': '50',
-            'limit': 1,
+            'commune': commune,
+            'distance': distance,
+            'grandDomaine': grandDomaine,
+            'motsCles': keyword,
+            'typeContrat': typeContrat,
+            'sort': sort
         }
         
         try:
